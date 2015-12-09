d3784afa287d7153908a2116f08391e78cf8dd01
joshuaphilippy <joshuaphilippy@gmail.com>
2015-12-08 21:34:58 -0600

    ^ adjusted the min support

--- comment_miner.R
+++ comment_miner.R
@@ -1,6 +1,7 @@
 require("arules")
 dataFile = file.choose()
 setwd(dirname(dataFile))
-x = read.transactions(dataFile, format = c("basket"), rm.duplicates = FALSE)
-a = apriori(x, parameter = list(supp = .2))
+x = read.transactions(dataFile, format = c("basket"), rm.duplicates = TRUE)
+suppValue = 1 / dim(x)[1] + 0.01 #min support > 1 per post
+a = apriori(x, parameter = list(supp = suppValue))
 inspect(a)
