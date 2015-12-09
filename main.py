9f5c9d9636d0e76203764757f259456891b86aeb
joshuaphilippy <joshuaphilippy@gmail.com>
2015-12-09 14:40:27 -0600

    ^tweaking

--- main.py
+++ main.py
@@ -9,11 +9,15 @@ user_agent = "Reddit Analayzer 1.0 by JP, Josh, and Gabie"
 #outfile = open("output.txt", 'w')
 
 def main():
-    if len(sys.argv) < 3:
-        print "Not Enough Arguments"
-        print "Usage: python2 %s subreddit_name #_of_posts" % sys.argv[0]
-        exit(0)
-
+    for path,dir,file in os.walk("./data"):
+        if len(sys.argv) < 3:
+            print "Not Enough Arguments"
+            print "Usage: python2 %s subreddit_name #_of_posts" % sys.argv[0]
+            exit(0)
+        if len(file) > 0:
+            print "remove files in %s to run this." % path
+            exit(0)
+        
     subreddit = sys.argv[1]
     r = praw.Reddit(user_agent=user_agent)
     posts = r.get_subreddit(subreddit).get_hot(limit=sys.argv[2])
