753e9a8ecb64f7b0f2b4eb56c127435013837930
JPisaBrony <jdpohl789@gmail.com>
2015-12-09 02:15:08 -0600

    ^ code for main project

--- main.py
+++ main.py
@@ -2,45 +2,41 @@ import sys
 import praw
 
 user_agent = "Reddit Analayzer 1.0 by JP, Josh, and Gabie"
-chars_to_ignore = ",.:;?[]{}_+=\\/|0123456789<>!@#$%^&*()-~"
-
-def read_file(file):
-    word_list = []
-    with open(file) as f:
-        word_list = f.read().splitlines()
-    return word_list
+outfile = open("output.txt", 'w')
 
 def main():
-    if len(sys.argv) < 4:
+    if len(sys.argv) < 3:
         print "Not Enough Arguments"
-        print "Usage: python2 %s subreddit_name #_of_posts_to_display words_to_exclude_file" % sys.argv[0]
+        print "Usage: python2 %s subreddit_name #_of_posts" % sys.argv[0]
         exit(0)
 
-    exclude_words = read_file(sys.argv[3])
     subreddit = sys.argv[1]
-    agent = praw.Reddit(user_agent=user_agent)
-    parse_subreddit(subreddit, agent, exclude_words)
-
-def parse_subreddit(subreddit, agent, exclude_words):
-    posts = agent.get_subreddit(subreddit).get_hot(limit=sys.argv[2])
+    r = praw.Reddit(user_agent=user_agent)
+    posts = r.get_subreddit(subreddit).get_hot(limit=sys.argv[2])
+    users = {}
+    users_to_post = {}
+    run_times = 0
     for x in posts:
-        print parse_post(x, exclude_words)[2:]
         flat = praw.helpers.flatten_tree(x.comments)
-        for comment in flat:
+        for c in flat:
             try:
-                print parse_post(comment.body.encode("ascii", "ignore"), exclude_words)
+                u = str(c.author).lower()
+                if u in users:
+                    users[u] += 1
+                else:
+                    users[u] = 1
+                users_to_post[u] = x.title
             except AttributeError:
                 pass
-
-def parse_post(post, words):
-    parsed_list = []
-    cur_words = str(post)
-    cur_words = cur_words.lower().split(" ")
-    for x in cur_words:
-        x = x.strip(chars_to_ignore)
-        if x not in words:
-            parsed_list.append(x)
-    return parsed_list
+        write_to_file(users_to_post)
+        users_to_post = {}
+        run_times += 1
+        if run_times >= int(sys.argv[2]):
+            break
+    
+def write_to_file(post):
+    for x in post:
+        outfile.write(x + " " + post[x].encode("ascii", "ignore") + "\n")
 
 if __name__ == "__main__":
     main()
